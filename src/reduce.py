2dbf7f5b90389069d674e77c2a0dce9b6bf77231
AstridDahl <88832493+AstridDahl@users.noreply.github.com>
2022-10-10 13:37:13 +0200

    reducevers1

--- src/reduce.py
+++ src/reduce.py
@@ -1,5 +1,6 @@
 """Reduce and accumulate module"""
 
+from tkinter import Y
 from typing import TypeVar, Callable
 A = TypeVar('A')
 B = TypeVar('B')
@@ -11,16 +12,48 @@ def reduce(f: Callable[[A], B], x: list[A]) -> B:
 
     >>> reduce(lambda x,y: x+y, [1, 2, 3])
     6
+    >>> reduce(lambda x,y: x*y, [2, 2, 3])
+    12
     """
     assert len(x) >= 2
-    ...  # FIXME
+
+    result = f(x[0],x[1])
+
+    for i in range(2,len(x)):
+        result = f(result,x[i])
+    
+    return result
 
 
-def accumulate(f: Callable[[A], A], x: list[A]) -> list[A]:
+
+def accumulate(f: Callable[[A], A], x: list[A], init_val) -> list[A]:
     """
     Accumulate f over list x.x
 
     >>> accumulate(lambda x,y: x+y, [1, 2, 3])
     [1, 3, 6]
     """
-    ...  # FIXME
+
+    result = f(init_val,x[0])
+
+    for i in range(1,len(x)):
+        result = f(result,x[i])
+    
+    return result
+
+
+print(accumulate(lambda x,y: x+y,[2, 2, 3],0))
+
+
+# x = 5
+
+# def f():
+#     x = 25
+#     print(x)
+
+# print(x)
+# f()
+# print(x)
+
+
+# print(f())
