9f4504401e4fb9458810a14083266fc45d923231
AstridDahl <88832493+AstridDahl@users.noreply.github.com>
2022-10-10 14:25:52 +0200

    reducevers2

--- src/reduce.py
+++ src/reduce.py
@@ -1,5 +1,6 @@
 """Reduce and accumulate module"""
 
+import re
 from tkinter import Y
 from typing import TypeVar, Callable
 A = TypeVar('A')
@@ -30,19 +31,23 @@ def accumulate(f: Callable[[A], A], x: list[A], init_val) -> list[A]:
     """
     Accumulate f over list x.x
 
-    >>> accumulate(lambda x,y: x+y, [1, 2, 3])
+    >>> accumulate(lambda x,y: x+y, [1, 2, 3],0)
     [1, 3, 6]
+    >>> accumulate(lambda x,y: x*y, [2, 2, 3],1)
+    [2, 4, 12]
     """
 
     result = f(init_val,x[0])
+    acc = [f(init_val,x[0])]
 
     for i in range(1,len(x)):
         result = f(result,x[i])
+        acc.append(result)
     
-    return result
+    return acc
 
 
-print(accumulate(lambda x,y: x+y,[2, 2, 3],0))
+# print(accumulate(lambda x,y: x*y,[2, 2, 3],1))
 
 
 # x = 5
